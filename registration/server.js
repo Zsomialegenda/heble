727212dda6d1effc280dfdc801e59cb90ab305ca
Zsomialegenda <sedlak.patrik.andras.2020@osztalyterem.katolikuskeri.hu>
2024-11-20 13:34:34 +0100

    Update server.js

--- registration/server.js
+++ registration/server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const PORT = 3000;
 const FILENAME = "output.json";
+const FILEPATH = path.join(__dirname, FILENAME);
 
 const LOCAL_HOST_ADDRESS = `http://localhost:${PORT}`;
 
@@ -25,33 +26,31 @@ app.post('/write-file', (req, res) => {
     return res.status(400).send('Invalid request: User data with a unique email is required');
   }
 
-  const filePath = path.join(__dirname, FILENAME);
+  ensureJsonFileIsValid(FILEPATH);
 
-  ensureJsonFileIsValid(filePath);
-
-  fs.readFile(filePath, 'utf-8', (readErr, data) => {
+  fs.readFile(FILEPATH, 'utf-8', (readErr, data) => {
     if (readErr) {
       console.error('Error reading file:', readErr);
       return res.status(500).send('Failed to read the file');
     }
 
-    let usersArray;
+    let arrayUserJSON;
     try {
-      usersArray = data.trim() ? JSON.parse(data) : []; 
+      arrayUserJSON = data.trim() ? JSON.parse(data) : []; 
     } catch (parseErr) {
       console.error('Error parsing JSON:', parseErr);
       return res.status(500).send('Invalid JSON in the file');
     }
 
-    const userExists = usersArray.some(user => user.user.email === content.user.email);
+    const userExists = arrayUserJSON.some(user => user.user.email === content.user.email);
 
     if (userExists) {
       return res.status(409).send('User already exists');
     }
 
-    usersArray.push(content);
+    arrayUserJSON.push(content);
 
-    fs.writeFile(filePath, JSON.stringify(usersArray, null, 2), (writeErr) => {
+    fs.writeFile(FILEPATH, JSON.stringify(arrayUserJSON, null, 2), (writeErr) => {
       if (writeErr) {
         console.error('Error writing to file:', writeErr);
         return res.status(500).send('Failed to write to the file');
@@ -61,6 +60,39 @@ app.post('/write-file', (req, res) => {
   });
 });
 
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send('Email and password are required');
+  }
+
+  ensureJsonFileIsValid(FILEPATH);
+
+  fs.readFile(FILEPATH, 'utf-8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).send('Failed to read the file');
+    }
+
+    let arrayUserJSON;
+    try {
+      arrayUserJSON = data.trim() ? JSON.parse(data) : [];
+    } catch (parseErr) {
+      console.error('Error parsing JSON:', parseErr);
+      return res.status(500).send('Invalid JSON in the file');
+    }
+
+    const user = arrayUserJSON.find(user => user.user.email === email && user.user.password === password);
+
+    if (user) {
+      res.status(200).send('Login successful');
+    } else {
+      res.status(401).send('Invalid email or password');
+    }
+  });
+});
 
 
 app.listen(PORT, () => {
