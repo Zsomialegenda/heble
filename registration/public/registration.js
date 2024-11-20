0bdaa31415a843e7650875e92d8bbe51c1b6c319
Zsomialegenda <sedlak.patrik.andras.2020@osztalyterem.katolikuskeri.hu>
2024-11-20 13:34:15 +0100

    Update and rename registerBeta.js to registration.js

--- registration/public/registration.js
+++ registration/public/registration.js
@@ -1,18 +1,19 @@
-const writeButton = document.getElementById('writeButton');
+const writeButton = document.getElementById('registerButton');
+
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var heble = Math.random() * 16 | 0, miau = c === 'x' ? heble : (heble & 0x3 | 0x8);
+    return miau.toString(16);
+  });
+}
 
 writeButton.addEventListener("click", () => {
-  function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var heble = Math.random() * 16 | 0, miau = c === 'x' ? heble : (heble & 0x3 | 0x8);
-      return miau.toString(16);
-    });
-  }
 
   const UUID = generateUUID();
   const firstName = document.getElementById('firstName').value;
   const secondName = document.getElementById('secondName').value;
   const gender = document.querySelector('select').value;
-  const email = document.getElementById('E-mail').value;
+  const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
   const content = {
@@ -35,9 +36,7 @@ writeButton.addEventListener("click", () => {
   })
     .then(response => {
       if (!response.ok) {
-        return response.text().then(text => {
-          throw new Error(text);
-        });
+        return response.text().then(text => { throw new Error(text); });
       }
       return response.text();
     })
