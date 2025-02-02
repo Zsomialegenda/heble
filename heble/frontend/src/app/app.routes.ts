4bb7083df93c4386bf52878aeebc3e64d8c7bbe1
FodorGergo7 <fodor.gergo.2020@osztalyterem.katolikuskeri.hu>
2025-02-02 16:50:21 +0100

    kisebb változtatások

--- heble/frontend/src/app/app.routes.ts
+++ heble/frontend/src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CoachingComponent } from './coaching/coaching.component';
+import { SigninComponent } from './signin/signin.component';
 import { FreecontentComponent } from './freecontent/freecontent.component';
-import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
+
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: "home", component: HomeComponent},
+    {path: "coaching", component: CoachingComponent},
     {path: "freecontent", component: FreecontentComponent},
-    {path: "signup", component: SignupComponent},
+    {path: "signin", component: SigninComponent},
     {path: "about", component: AboutComponent},
     {path: "contacts", component: ContactsComponent},
     {path: "**", component: HomeComponent}
