fda8abab6d1e73753b17743dbb64979b9143dad0
FodorGergo7 <fodor.gergo.2020@osztalyterem.katolikuskeri.hu>
2025-02-05 19:22:55 +0100

    tartalmak bővítés és kisebb design

--- heble/frontend/src/app/contentsleeping/contentsleeping.component.ts
+++ heble/frontend/src/app/contentsleeping/contentsleeping.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-contentsleeping',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './contentsleeping.component.html',
   styleUrl: './contentsleeping.component.css'
 })
